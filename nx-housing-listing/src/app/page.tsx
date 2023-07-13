c455467642c090e8fe2a2fb810e50f5acce2da1d
DavidValent1ne <122053126+DavidValent1ne@users.noreply.github.com>
2023-07-13 04:18:02 -0500

    All Features added
    added a working table with filters, sorts, searching,  wiki links, images also added a model page with info on the launches the crew member has been on. One change i would like to make is to move all the api fetch functions to the mian page to reduce api calls,

--- nx-housing-listing/src/app/page.tsx
+++ nx-housing-listing/src/app/page.tsx
@@ -1,15 +1,4 @@
-import { CrewTable } from "@/components/CrewTable";
-// import { useState, useReducer } from "react";
-// This would be the second error you will get... next page does not support react hooks because its a server side rendered app
-// meaning this stuff right here is all "backend" stuff and you can not use react hooks here
-
-// import ReactDOM from 'react-dom/client';
-// you do not need to import react-dom because nextjs does it for you and it will cause an error if you do
-// This was the main error but after you fix this you will get another error
-
-/*
-https://github.com/r-spacex/SpaceX-API/blob/master/docs/crew/v4/all.md
-*/
+import { CrewTable } from "@/components/CrewTable"
 export type Crew = {
   name: string;
   agency: string;
@@ -36,34 +25,32 @@ async function fetchSpaceXCrew() {
     console.error("An error occurred:", error);
   }
 }
+export async function getCrew(){
+  const crewInfo: Crew[] = await fetchSpaceXCrew();
+  return crewInfo
+}
 
 export default async function Home() {
   const crewInfo: Crew[] = await fetchSpaceXCrew();
+
 
   return (
     <main className="">
       <h1 className="text-4xl font-bold text-center py-4">
         SpaceX Crew Members
       </h1>
-      <section>
+      <section style={{ margin: '0 auto' }}>
         {/* 
         SOME STUFF TO DO:
-        1. Lets start by making this a table where we can see all of the crews information
-        2. We should be able to sort by name, agency, status (https://tanstack.com/table/v8) WE use this in our company.
-        3. We should be able to filter by agency
-        4. When we click on the image, we should be able to see a modal with more information about the crew member specifically more information about the launches they have been on.
-        5. We should be able to search by name
-        6. when we click on wiki we should open a new tab with the wikipedia page
-        7. We should be able to see the number of launches they have been on
+        1. Lets start by making this a table where we can see all of the crews information  **DONE**
+        2. We should be able to sort by name, agency, status (https://tanstack.com/table/v8) WE use this in our company. **DONE**
+        3. We should be able to filter by agency **DONE**
+        4. When we click on the image, we should be able to see a modal with more information about the crew member specifically more information about the launches they have been on. **DONE**
+        5. We should be able to search by name **DONE**
+        6. when we click on wiki we should open a new tab with the wikipedia page **DONE**
+        7. We should be able to see the number of launches they have been on **DONE
         */}
-
-        {/* THIS CAN BE REMOVED SINCE WE ADDED A TABLE */}
-        {/* {crewInfo?.map((crew) => (
-          <div key={crew.id} className="w-auto h-auto">
-            <Image src={crew.image} alt={crew.name} width={200} height={200} />
-          </div>
-        ))} */}
-        <CrewTable crewInfo={crewInfo} />
+        <CrewTable crewInfo={crewInfo}/>
       </section>
     </main>
   );
--- nx-housing-listing/src/components/CrewTable.tsx
+++ nx-housing-listing/src/components/CrewTable.tsx
@@ -1,97 +1,232 @@
 // You need to tell nextjs "hey this is a react client side component" by adding the following comment
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState, useEffect } from "react";
+
+
 import {
+  Column,
+  Table,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import { Crew } from "@/app/page";
-const columnHelper = createColumnHelper<Crew>();
 
+const columnHelper = createColumnHelper<Crew>();
 const columns = [
   columnHelper.accessor("name", {
     cell: (info) => <i>{info.getValue()}</i>,
+    id: 'name',
     header: () => <span>Name</span>,
-    footer: (info) => info.column.id,
   }),
   columnHelper.accessor("agency", {
-    header: () => "agency",
+    header: () => "Agency",
     cell: (info) => info.renderValue(),
-    footer: (info) => info.column.id,
+    id: 'agency',
   }),
   columnHelper.accessor("status", {
-    header: () => <span>Visits</span>,
-    footer: (info) => info.column.id,
-  }),
-  columnHelper.accessor("launches", {
-    header: "Status",
-    footer: (info) => info.column.id,
-  }),
+    header: () => <span style={{width: '100px'}}>Status</span>,
+    id: 'status',
+    }),
   columnHelper.accessor("wikipedia", {
-    header: "wiki",
-    footer: (info) => info.column.id,
+    header: "Wikipedia",
+    cell: ({ cell }) => <a className="link-style" href={cell.getValue()} >More Info</a>,
+    id: 'wikipedia', 
   }),
+  columnHelper.accessor("image", {
+    header: "Crew",
+    
+    cell: ({ cell }) => (
+      <a href={`/model/${cell.row.original.id}`}>
+        <img src={cell.getValue()} height="150" width="150" alt="image" />
+      </a>
+    ),
+    id: 'crew',
+  })
 ];
+
+
 type CrewTableProps = {
-  crewInfo: Crew[];
-};
+  crewInfo: Crew[]
+}
+
 export function CrewTable({ crewInfo }: CrewTableProps) {
   // This is not an error but you dont need to spread the array into a new array
-  const [data, setData] = useState(crewInfo);
+  const [data, setData] = useState(crewInfo)
+  const [sorting, setSorting] = useState([])
+  const [filtering, setFiltering] = useState('')
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+
+    state: {
+      sorting: sorting,
+      globalFilter: filtering,
+    },
+    onSortingChange: setSorting as any,
+    onGlobalFilterChange: setFiltering,
   });
+
   return (
-    <table>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        {table.getFooterGroups().map((footerGroup) => (
-          <tr key={footerGroup.id}>
-            {footerGroup.headers.map((header) => (
-              <th key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.footer,
-                      header.getContext()
-                    )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+    <div>
+      <table style={{ margin: '0 auto' }}>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  <div
+                      {...{
+                        className: ['name', 'agency'].includes(header.id) && header.column.getCanSort()
+                          ? 'cursor-pointer select-none'
+                          : 'select-none',
+                        onClick: ['name', 'agency'].includes(header.id) && header.column.getCanSort()
+                          ? header.column.getToggleSortingHandler()
+                          : undefined,
+                      }}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {{
+                        asc: ' 🔼',
+                        desc: ' 🔽',
+                      }[header.column.getIsSorted() as string] ?? null}
+                       
+                    </div>
+
+                    <div>
+                      {['name'].includes(header.id) && (
+                        <FilterInput column={header.column} table={table} droppable={false} />
+                      )}
+                      {['agency', 'status'].includes(header.id) && (
+                        <FilterInput column={header.column} table={table} droppable={true} />
+                      )}
+                      {!['name', 'agency', 'status'].includes(header.id) && <br />}
+                    </div>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
+}
+
+export function FilterInput({
+  column,
+  table,
+  droppable = false,
+}:{
+  column: Column<any, any>
+  table: Table<any>
+  droppable?: boolean
+}) {
+  const flatRows = table.getFilteredRowModel().flatRows;
+  const uniqueValues = new Set<any>();
+
+  flatRows.forEach((row) => {
+    const value = row.getValue(column.id);
+    uniqueValues.add(value);
+  });
+  const sortedUniqueValues = Array.from(uniqueValues).sort()
+
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+  const columnValueAsString = column.getFilterValue() as string;
+
+  return (
+    <>
+      <datalist id={column.id + 'list'}>
+        {sortedUniqueValues.slice(0, 100).map((value: any) => (
+          <option value={value} key={value} />
+        ))}
+      </datalist>
+
+      {droppable ? (
+        <select
+        value={columnValueAsString ?? ''}
+        style={{
+          color: '#000',
+          height: '23px',
+          width: '100px'
+        }}
+        onChange={(e) => column.setFilterValue(e.target.value)}
+      >
+        <option value="">All</option>
+        {sortedUniqueValues.map((value: string) => (
+          <option value={value} key={value}>
+            {value}
+          </option>
+        ))}
+      </select>
+      ) : (
+      <DebouncedInput
+        type="text"
+        value={(columnFilterValue ?? '') as string}
+        onChange={value => column.setFilterValue(value)}
+        placeholder={`Search... (${sortedUniqueValues.length})`}
+        style={{
+          color: '#000',
+          height: '23px',
+        }}
+        list={column.id + 'list'}
+      />
+      )}
+      <div/>
+    </>
+  )
+}
+
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 250,
+  ...props
+}: {
+  value: string | number
+  onChange: (value: string | number) => void
+  debounce?: number
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [value])
+
+  return (
+    <input {...props} value={value} onChange={e => setValue(e.target.value)} />
+  )
 }
